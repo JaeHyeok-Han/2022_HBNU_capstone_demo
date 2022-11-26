6e58bf23bad16c41557a0c3e7f8a9255f1c1351d
JaeHyeok-Han <hjh0633@gmail.com>
2022-11-26 18:54:24 +0900

    Feat: API 요청 코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
+const fetchAnalyze = async (text: string) => {
+  const response = await fetch("http://203.230.103.35:3000", {
+    method: "POST",
+    body: JSON.stringify({
+      text,
+    }),
+  });
+  if (response.ok) {
+    const jsonRes = await response.json();
+    console.log(jsonRes);
+  } else {
+    console.log(response.statusText);
+  }
+};
+
 function App() {
   return <div className="App"></div>;
 }
